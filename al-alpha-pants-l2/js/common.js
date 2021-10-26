72f31cf7e568dfad6e34bcb4628072df1ef50bc6
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2021-10-26 14:33:12 +0300

    offers-820

---
+++ al-alpha-pants-l2/js/common.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper('.sect5__slider', {
+  pagination: {
+    el: '.sect5__pag',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoHeight: true,
+  spaceBetween: 40
+});
+var swiper2 = new Swiper('.sect7__slider', {
+  pagination: {
+    el: '.sect7__pag',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoHeight: true,
+  spaceBetween: 40
+});
+console.log('made by https://github.com/DanzigWebb')
