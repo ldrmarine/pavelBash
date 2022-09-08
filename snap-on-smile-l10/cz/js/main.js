9e281eb0815eedd7ec361dbfb27265a7284f68b6
Pavel Bashkeev <pavelbashkeev@gmail.com>
2022-09-08 11:52:23 +0300

    update

--- snap-on-smile-l10/cz/js/main.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function($) {
-    $(".scroll_to_form").on("touchend, click", function() {
-        $('body,html').animate({ scrollTop: $('.orderForm').offset().top }, 400);
-    });
-});
